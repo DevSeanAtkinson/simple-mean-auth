24559065b47b7e9820a1c00ff728d8f9f7cff51d
Sean Atkinson <Sean Atkinson>
2016-03-31 19:33:58 -0400

    added autherntication routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -7,17 +7,17 @@ var User = require('../models/user.model.js');
 
 router.post('/register', function(req, res) {
   User.register(new User({ username: req.body.username }),
-req.body.password, function(err, account) {
-if (err) {
-return res.status(500).json({
-err: err
-});
-}
+    req.body.password, function(err, account) {
+      if (err) {
+      return res.status(500).json({
+        err: err
+      });
+    }
 passport.authenticate('local')(req, res, function () {
-return res.status(200).json({
-status: 'Registration successful!'
-});
-});
+  return res.status(200).json({
+    status: 'Registration successful!'
+    });
+  });
 });
   //registration route for signing up users
 });
@@ -25,14 +25,48 @@ status: 'Registration successful!'
 router.post('/login', function(req, res, next) {
   //login route for logging in existing users
   // What do you notice about this function?
+  passport.authenticate('local', function(err, user, info){
+    if(err){
+      return next(err);
+    }
+    if(!user){
+      return res.status(401).json({
+        err: info
+      });
+   }
+
+   req.logIn(user, function(err){
+     if(err){
+       return res.status(500).json({
+         err: 'Could not log in user'
+       });
+     }
+     res.status(200).json({
+       status: 'Login successful'
+     });
+
+   });
+ })(req, res, next);
 });
 
 router.get('/logout', function(req, res) {
   //Logout route
+  req.logout();
+  res.status(200).json({
+    status: 'Bye'
+  });
 });
 
 router.get('/status', function(req, res) {
   //Why do we need a status route?
+  if(!req.isAuthenticated()){
+    return res.status(200).json({
+      status: false,
+    })
+  }
+  res.status(200).json({
+    status: true
+  });
 });
 
 
